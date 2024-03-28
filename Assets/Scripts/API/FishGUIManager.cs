be0e4d37c167201f4b2ca279eeec855e7cf49560
dummy-ie <115205154+dummy-ie@users.noreply.github.com>
2024-03-29 03:14:24 +0800

    Progress on web API connection
    Fish scene now displays fish icons
    Added newtonsoft.json package
    Added FishModel.cs (todo: Model for the rest of the data)

---
+++ Assets/Scripts/API/FishGUIManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UIElements;
+
+public class FishGUIManager : MonoBehaviour
+{
+    private VisualElement _root;
+    private VisualElement _listView;
+    List<FishModel> _fishList = new();
+
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        this._root = this.GetComponent<UIDocument>().rootVisualElement;
+        this._listView = _root.Q<VisualElement>(className: "catchable-list");
+
+
+        StartCoroutine(StartFillList());
+    }
+
+    private IEnumerator StartFillList()
+    {
+
+        WWWForm form = new();
+
+        using UnityWebRequest handler = UnityWebRequest.Post("http://localhost/sqlconnect/AnimalCrossingBuddy/fish.php", form);
+        yield return handler.SendWebRequest();
+
+        string[] result = handler.downloadHandler.text.Split('\t');
+
+        if (result[0] == "0")
+        {
+            for (int i = 1; i < result.Length - 1; i++)
+            {
+                FishModel fish = JsonConvert.DeserializeObject<FishModel>(result[i]);
+                _fishList.Add(fish);
+            }
+
+            StartCoroutine(CreateIconList());
+        }
+        else { Debug.LogError("php failed. [ERROR] : " + handler.error); }
+    }
+
+    private IEnumerator DownloadTexture(string url, System.Action<Texture2D> callback)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(url);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+
+        }
+        else
+        {
+            DownloadHandlerTexture response = (DownloadHandlerTexture)request.downloadHandler;
+            callback(response.texture);
+        }
+    }
+
+    private IEnumerator CreateIconList()
+    {
+        foreach (var fish in _fishList)
+        {
+            Texture2D icon = new(0, 0);
+            
+            yield return StartCoroutine(DownloadTexture(fish.IconImage, (tex) => icon = tex));
+
+            VisualElement newIcon = new();
+            newIcon.AddToClassList("portraits");
+            newIcon.style.backgroundImage = new StyleBackground(icon);
+            _listView.Add(newIcon);
+        }
+
+    }
+}
--- Assets/Scripts/Models/FishModel.cs
+++ Assets/Scripts/Models/FishModel.cs
@@ -1,18 +1,110 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 using UnityEngine;
 
-[Serializable]
 public class FishModel
 {
+    [JsonProperty("Id")]
     public string Id { get; set; }
-    public string IconImageLink { get; set; }
-    public string CritterpediaImageLink { get; set; }
+
+    [JsonProperty("Name")]
+    public string Name { get; set; }
+
+    [JsonProperty("Icon Image Link")]
+    public string IconImage { get; set; }
+
+    [JsonProperty("Critterpedia Image Link")]
+    public string CritterpediaImage { get; set; }
+
+    [JsonProperty("Sell Price")]
     public int SellPrice { get; set; }
+
+    [JsonProperty("Location")]
     public string Location { get; set; }
+
+    [JsonProperty("Shadow Size")]
     public string ShadowSize { get; set; }
+
+    [JsonProperty("Catch Difficulty")]
     public string CatchDifficulty { get; set; }
+
+    [JsonProperty("Total Catches")]
     public int TotalCatches { get; set; }
+
+    [JsonProperty("NH Jan")]
+    public string NHJan { get; set; }
+
+    [JsonProperty("NH Feb")]
+    public string NHFeb { get; set; }
+
+    [JsonProperty("NH Mar")]
+    public string NHMar { get; set; }
+
+    [JsonProperty("NH Apr")]
+    public string NHApr { get; set; }
+
+    [JsonProperty("NH May")]
+    public string NHMay { get; set; }
+
+    [JsonProperty("NH Jun")]
+    public string NHJun { get; set; }
+
+    [JsonProperty("NH Jul")]
+    public string NHJul { get; set; }
+
+    [JsonProperty("NH Aug")]
+    public string NHAug { get; set; }
+
+    [JsonProperty("NH Sep")]
+    public string NHSep { get; set; }
+
+    [JsonProperty("NH Oct")]
+    public string NHOct { get; set; }
+
+    [JsonProperty("NH Nov")]
+    public string NHNov { get; set; }
+
+    [JsonProperty("NH Dec")]
+    public string NHDec { get; set; }
+
+    [JsonProperty("SH Jan")]
+    public string SHJan { get; set; }
+
+    [JsonProperty("SH Feb")]
+    public string SHFeb { get; set; }
+
+    [JsonProperty("SH Mar")]
+    public string SHMar { get; set; }
+
+    [JsonProperty("SH Apr")]
+    public string SHApr { get; set; }
+
+    [JsonProperty("SH May")]
+    public string SHMay { get; set; }
+
+    [JsonProperty("SH Jun")]
+    public string SHJun { get; set; }
+
+    [JsonProperty("SH Jul")]
+    public string SHJul { get; set; }
+
+    [JsonProperty("SH Aug")]
+    public string SHAug { get; set; }
+
+    [JsonProperty("SH Sep")]
+    public string SHSep { get; set; }
+
+    [JsonProperty("SH Oct")]
+    public string SHOct { get; set; }
+
+    [JsonProperty("SH Nov")]
+    public string SHNov { get; set; }
+
+    [JsonProperty("SH Dec")]
+    public string SHDec { get; set; }
+
+    [JsonProperty("Description")]
     public string Description { get; set; }
 }
