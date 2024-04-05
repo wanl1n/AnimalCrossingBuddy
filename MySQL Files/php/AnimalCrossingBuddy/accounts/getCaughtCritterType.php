ec18d33f65cbf3431d5ececf0e3f0f4817319f61
FinalSpeedZ <85392584+FinalSpeedZ@users.noreply.github.com>
2024-04-05 20:01:14 +0800

    Update getCaughtCritterType.php

--- MySQL Files/php/AnimalCrossingBuddy/accounts/getCaughtCritterType.php
+++ MySQL Files/php/AnimalCrossingBuddy/accounts/getCaughtCritterType.php
@@ -9,12 +9,15 @@
 	$name = $_POST["name"];
 	trim($name);
 
-	$getTypeQuery = "SELECT * FROM `caught_critters` WHERE Name = '" . $name . "' LIMIT 1;";
+	$getTypeQuery = "SELECT Type FROM `caught_critters` WHERE Name = '" . $name . "' LIMIT 1;";
 
 	echo $getTypeQuery;
 
 	$typeQuery = mysqli_query($connection, $getTypeQuery) or die("[2] : SEARCH TYPE QUERY failed.");
 	
-    echo "0\t" . $typeQuery;
+	if ($row = mysqli_fetch_assoc($typeQuery)) {
+        // Use the fetched value in the echo statement
+        echo "0\t" . $row['Type'];
+    } 
 
 	?>
